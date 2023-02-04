d06466a28c652d75fafffee068a5df1e0681f9a3
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-02-04 14:17:58 -0500

    what?

--- shared/config.lua
+++ shared/config.lua
@@ -1,14 +1,14 @@
 Config = {}
 
-Config.UseRPName = true 							    -- If set to true, it uses either esx-legacy or qb-core built-in function to get players' RP name
+Config.UseRPName = true                                 -- If set to true, it uses either esx-legacy or qb-core built-in function to get players' RP name
 
 Config.LetPlayersChangeVisibilityOfRadioList = true     -- Let players to toggle visibility of the list
-Config.RadioListVisibilityCommand = "radiolist" 	    -- Only works if Config.LetPlayersChangeVisibilityOfRadioList is set to true
+Config.RadioListVisibilityCommand = "radiolist"         -- Only works if Config.LetPlayersChangeVisibilityOfRadioList is set to true
 Config.HideRadioListVisibilityByDefault = false         -- If set to true and a player joins the server, don't show the radio list until the player execute the Config.RadioListVisibilityCommand command
 
-Config.LetPlayersSetTheirOwnNameInRadio = true		    -- Let players to customize how their name is displayed on the list
-Config.ResetPlayersCustomizedNameOnExit = true		    -- Only works if Config.LetPlayersSetTheirOwnNameInRadio is set to true - Removes customized name players set for themselves on their server exit
-Config.RadioListChangeNameCommand = "nameinradio" 	    -- Only works if Config.LetPlayersSetTheirOwnNameInRadio is set to true
+Config.LetPlayersSetTheirOwnNameInRadio = true          -- Let players to customize how their name is displayed on the list
+Config.ResetPlayersCustomizedNameOnExit = true          -- Only works if Config.LetPlayersSetTheirOwnNameInRadio is set to true - Removes customized name players set for themselves on their server exit
+Config.RadioListChangeNameCommand = "nameinradio"       -- Only works if Config.LetPlayersSetTheirOwnNameInRadio is set to true
 
 Config.LetPlayersChangeRadioChannelsName = true         -- Let players to change the name of the radio channels **they are currently joined in**
 Config.ModifyRadioChannelNameCommand = "nameofradio"    -- Changes the name of the radio channel **that the player is currently joined in** => this is a validation to prevent normal players from modifying the name of the restricted channels they don't have access to(such as police, & etc)
