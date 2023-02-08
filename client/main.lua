5e3812b86d3c6ed2fc5339ee43c98439797a8312
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-02-08 02:32:19 -0500

    Revert "fix(client): error in radio list members get doubled"
    This reverts commit c089fe7401b4d8c97ee92b8f17a9c7fb104e1c0d.

--- client/main.lua
+++ client/main.lua
@@ -36,7 +36,6 @@ RegisterNetEvent("pma-voice:removePlayerFromRadio", function(playerId)
 end)
 
 RegisterNetEvent("pma-voice:syncRadioData", function()
-    if currentRadioChannel and currentRadioChannel > 0 then return end
     closeTheRadioList()
     local playersInRadio
     playersInRadio, currentRadioChannel, currentRadioChannelName = callback.await(Shared.Callback.getPlayersInRadio, false)
